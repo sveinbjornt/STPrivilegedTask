395676686c8320ba5ec7b5f3c1d0bb1c4378a289
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-03-15 22:03:13 +0000

    Updated project settings, code formatting, comments, copyright + minor refactoring

--- PrivilegedTaskExample/PrivilegedTaskExample/AppDelegate.m
+++ PrivilegedTaskExample/PrivilegedTaskExample/AppDelegate.m
@@ -1,30 +1,29 @@
 /*
- # PrivilegedTaskExample
- # Copyright (C) 2009 Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
- #
- # BSD License
- # Redistribution and use in source and binary forms, with or without
- # modification, are permitted provided that the following conditions are met:
- #     * Redistributions of source code must retain the above copyright
- #       notice, this list of conditions and the following disclaimer.
- #     * Redistributions in binary form must reproduce the above copyright
- #       notice, this list of conditions and the following disclaimer in the
- #       documentation and/or other materials provided with the distribution.
- #     * Neither the name of Sveinbjorn Thordarson nor that of any other
- #       contributors may be used to endorse or promote products
- #       derived from this software without specific prior written permission.
- #
- # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- # DISCLAIMED. IN NO EVENT SHALL  BE LIABLE FOR ANY
- # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+   Copyright (C) 2008-2020 Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
+   
+   BSD License
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+       * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+       * Neither the name of the copyright holder nor that of any other
+       contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+   
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL  BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import "AppDelegate.h"
 #import "STPrivilegedTask.h"
@@ -42,8 +41,8 @@
 }
 
 - (IBAction)runNSTask:(id)sender {
-    
     NSString *cmd = [self.commandTextField stringValue];
+    
     if ([self isValidShellCommand:cmd] == NO) {
         NSBeep();
         NSAlert *alert = [[NSAlert alloc] init];
@@ -81,7 +80,6 @@
 }
 
 - (IBAction)runSTPrivilegedTask:(id)sender {
-    
     NSString *cmd = [self.commandTextField stringValue];
     if ([self isValidShellCommand:cmd] == NO) {
         NSBeep();
@@ -103,7 +101,7 @@
     [privilegedTask setArguments:components];
     [privilegedTask setCurrentDirectoryPath:[[NSBundle mainBundle] resourcePath]];
     
-    //set it off
+    // Set it off
     OSStatus err = [privilegedTask launch];
     if (err != errAuthorizationSuccess) {
         if (err == errAuthorizationCanceled) {
@@ -117,9 +115,9 @@
     
     [privilegedTask waitUntilExit];
     
-    // Success!  Now, start monitoring output file handle for data
+    // Success! Now, read the output file handle for data
     NSFileHandle *readHandle = [privilegedTask outputFileHandle];
-    NSData *outputData = [readHandle readDataToEndOfFile];
+    NSData *outputData = [readHandle readDataToEndOfFile]; // Blocking call
     NSString *outputString = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding];
     [self.outputTextField setString:outputString];
     
